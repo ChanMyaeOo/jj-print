00577c33558dcc7676b570a505a9c2b46f9f935b
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2021-08-04 00:02:00 +0630

    added navigation for responsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,24 @@
 import React from "react";
+import MenuIcon from '@material-ui/icons/Menu';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import CloseIcon from '@material-ui/icons/Close';
 import useStyles from "./styles";
 import Logo from '../../images/logo.png'
 
 const Header = () => {
     const classes = useStyles();
+
+    const handleOverlayClose = () => {
+        const overlay = document.getElementById('overlay')
+        overlay.style.display = 'none'
+    }
+
+    const handleOverlayOpen = () => {
+        const overlay = document.getElementById('overlay')
+        overlay.style.display = 'block'
+    }
     return (
         <div>
             <div className={classes.barWrap}>
@@ -17,6 +32,7 @@ const Header = () => {
                     <li>£ 0</li>
                     <li>Card</li>
                 </ul>
+                <div>Ad Print Delivered in High Quality</div>
             </div>
             <div className={classes.headerWrap}>
                 <img src={Logo} alt="logo" className={classes.logo} />
@@ -89,6 +105,89 @@ const Header = () => {
                     <li>roller banners</li>
                     <li>london book</li>
                 </ul>
+            </div>
+
+            {/* For Small Devices */}
+            <div className={classes.resHeaderWrap}>
+                <div className={classes.resHeader}>
+                    <MenuIcon onClick={handleOverlayOpen}/>
+                    <img src={Logo} alt="logo" className={classes.resLogo} />
+                    <div>£ 0</div>
+                </div>
+            </div>
+
+            <div className={classes.overlay} id="overlay">
+                <CloseIcon className={classes.overlayClose} onClick={handleOverlayClose}/>
+                <div className={classes.sideBar}>
+                    <img src={Logo} alt="logo" className={classes.sideBarLogo}/>
+
+                    <ul className={classes.sidebarNav}>
+                        <li className={classes.sideBarProducts}>
+                            All products
+                            <div className={classes.productsWrap}>
+                                <h5>advertising materials</h5>
+                                <div>Business Cards</div>
+                                <div>Digital Flyers</div>
+                                <div>Litho Flyers</div>
+                                <div>Promotional Items</div>
+
+                                <h5>business/personal stationery</h5>
+                                <div>Calenders</div>
+                                <div>Complient Slips</div>
+                                <div>Envelopes</div>
+                                <div>Folders</div>
+                                <div>Invoice Books</div>
+                                <div>Labels/Stickers</div>
+                                <div>Letterheads</div>
+                                <div>Notepads</div>
+                                <div>Stamps</div>
+
+                                 <h5>advertising products</h5>
+                                <div>Brochures &amp; Magazines</div>
+                                <div>Foil Press</div>
+                                <div>Posters</div>
+                                <div>Books</div>
+                                <div>Hard Cover Menu</div>
+                                <div>Lether Cover Menu</div>
+                                <div>Paper Menu</div>
+
+                                 <h5>customized items</h5>
+                                <div>Awards &amp; Trophies</div>
+                                <div>Roller Banners</div>
+                                <div>Canvas &amp; Framed Print</div>
+                                <div>Mugs</div>
+                                <div>T-shirts</div>
+                                <div>Vinyl</div>
+                                <div>Wedding Cards</div>
+                            </div>
+                        </li>
+                        <li>business cards</li>
+                        <li className={classes.sideBarFlyers}>
+                            flyers
+                            <div className={classes.sideBarSubFlyers}>
+                                <div>Litho Flyers</div>
+                                <div>Digital Flyers</div>
+                            </div>
+                        </li>
+                        <li>envelopes</li>
+                        <li>invoice books</li>
+                        <li>stamps</li>
+                        <li>roller banners</li>
+                        <li>london book</li>
+                    </ul>
+
+                    <div className={classes.sidebarSocial}>
+                        <FacebookIcon />
+                        <InstagramIcon />
+                        <TwitterIcon />
+                    </div>
+
+                    <div className={classes.sidebarContact}>
+                         <div>Mon. - Fri. 09:30 - 18:00</div>
+                         <div>Sat. 10:00 - 18:00</div>
+                        <div className={classes.sidebarPhone}>020 7289 9000</div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -3,7 +3,12 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
     barWrap: {
         background: '#000',
-        width: '100%'
+        width: '100%',
+        "& > div": {
+            color: '#fff',
+            width: '100%',
+            display: 'none'
+        }
     },
     headerBar: {
         display: 'flex',
@@ -102,9 +107,152 @@ const useStyles = makeStyles((theme) => ({
         padding: '10px 0'
     },
 
+    // for responsive header
+    resHeaderWrap: {
+        width: '100%',
+        display: 'none',
+    },
+    resHeader: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '0 15px',
+        borderBottom: '1px solid #ddd'
+    },
+    resLogo: {
+        width: '75px',
+        height: '66px'
+    },
+
+    // for overlay
+    overlayClose: {
+        color: '#f2f2f2',
+        fontSize: '35px',
+        position: 'fixed',
+        top: '10px',
+        right: '10px'
+    },
+    overlay: {
+        position: 'fixed',
+        left: 0,
+        top: 0,
+        width: '100%',
+        height: '100%',
+        background: 'rgba(0,0,0,0.5)',
+        display: 'none'
+    },
+    sideBar: {
+        width: '35%',
+        background: '#f2f2f2',
+        height: '100%',
+        overflowY: 'scroll'
+    },
+    sideBarLogo: {
+        width: '95px',
+        height: '75px',
+        display: 'block',
+        margin: '0 auto',
+        padding: '25px 0'
+    },
+    sidebarNav: {
+        "& > li": {
+            padding: '15px',
+            listStyle: 'none',
+            borderBottom: '1px solid #ddd',
+            textTransform: 'uppercase',
+        }
+    },
+    productsWrap: {
+        lineHeight: '1.5rem',
+        display: 'none',
+        "& > h5": {
+            color: '#008b8b',
+            fontSize: '16px',
+            marginTop: '10px',
+        },
+        "& > div": {
+            textTransform: 'none',
+            color: '#666'
+        }
+    },
+    sideBarProducts: {
+        
+        "&:hover": {
+            background: '#ddd',
+             "& $productsWrap": {
+            display: 'block'
+        }
+        }
+    },
+    sideBarFlyers: {
+        lineHeight: '1.5rem',
+        "&:hover": {
+            background: '#ddd',
+            "& $sideBarSubFlyers": {
+                display: 'block'
+            }
+        }
+    },
+    sideBarSubFlyers: {
+        marginTop: '10px',
+        display: 'none',
+        "& > div": {
+            textTransform: 'none',
+            color: '#666'
+        }
+    },
+    sidebarSocial: {
+        display: 'flex',
+        justifyContent: 'space-around',
+        width: '60%',
+        padding: '10px 0',
+        margin: '0 auto'
+    },
+    sidebarContact: {
+        marginTop: '20px',
+        paddingLeft: '15px'
+    },
+
+    sidebarPhone: {
+        color: '#008b8b',
+        fontWeight: '600',
+        fontSize: '18px',
+        marginTop: '10px'
+    },
+
+    // for responsive
     [theme.breakpoints.down('md')]: {
         productsNavWrap: {
             width: '930px'
+        }
+    },
+
+    [theme.breakpoints.down('sm')]: {
+        barWrap: {
+            "& > div": {
+                display: 'block',
+                padding: '5px 0',
+                textAlign: 'center',
+                fontWeight: 'bold'
+            }
+        },
+        headerBar: {
+            display: 'none'
+        },
+        headerWrap: {
+            display: 'none'
+        },
+        navWrap: {
+            display: 'none'
+        },
+        resHeaderWrap: {
+            display: 'block'
+        }
+    },
+
+    [theme.breakpoints.down('xs')]: {
+        sideBar: {
+            width: '60%'
         }
     }
     
