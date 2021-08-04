eab14fbdd46c2451e7fc1410a5afd07f39787bad
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2021-08-04 17:00:43 +0630

    added hero section

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import useStyles from './styles'
+
+
+const Hero = () => {
+    const classes = useStyles();
+    return (
+        <div className={classes.heroWrap}>
+            <div className={classes.heroContent}>
+                <h2>CUSTOM-MADE FLYERS</h2>
+                <h3>Flyers available at excellent prices, in various different colours and paper types.</h3>
+            </div> 
+        </div>
+    )
+}
+
+export default Hero
---
+++ src/components/hero/styles.js
@@ -0,0 +1,62 @@
+import { makeStyles } from "@material-ui/core/styles";
+import HeroImg from '../../images/flyer.jpg'
+
+const useStyles = makeStyles((theme) => ({
+    heroWrap: {
+        background: '#f2f2f2',
+        paddingTop: '15px'
+    },
+    heroContent: {
+        background: `linear-gradient( rgba(0, 0, 0, 0.6) 100%, rgba(0, 0, 0, 0.5) 100%),url(${HeroImg})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center center',
+        backgroundSize: 'cover',
+        width: '85%',
+        height: '400px',
+        margin: '0 auto',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        "& > h2": {
+            fontSize: '48px',
+            color: '#f2f2f2',
+            paddingBottom: '20px',
+            textAlign: 'center'
+        },
+        "& > h3": {
+            fontSize: '23px',
+            letterSpacing: '1px',
+            fontWeight: 'normal',
+            color: '#f2f2f2',
+            textAlign: 'center'
+        }
+    },
+    [theme.breakpoints.down('md')]: {
+        heroContent: {
+            "& > h2": {
+                fontSize: '40px'
+            },
+            "& > h3": {
+                fontSize: '20px'
+            }
+        }
+    },
+    [theme.breakpoints.down('sm')]: {
+        heroContent: {
+            width: '95%'
+        }
+    },
+    [theme.breakpoints.down('xs')]: {
+         heroContent: {
+            "& > h2": {
+                fontSize: '27px'
+            },
+            "& > h3": {
+                fontSize: '17px'
+            }
+        }
+    },
+
+}));
+export default useStyles;
